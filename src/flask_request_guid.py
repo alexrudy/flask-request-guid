325028816e014b3457e46b12f8fc4128831b639a
Alex Rudy <opensource@alexrudy.net>
2022-06-09 10:32:55 -0700

    Documentation setup

--- src/flask_request_guid.py
+++ src/flask_request_guid.py
@@ -9,13 +9,20 @@ import uuid
 from typing import Optional
 from flask import Flask, request, Response, _app_ctx_stack
 
+__all__ = ["FlaskRequestGUID"]
+
 
 class FlaskRequestGUID:
+    """
+    A flask extension to add a unique ID to each request.
+    """
+
     def __init__(self, app: Optional[Flask] = None) -> None:
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app: Flask) -> None:
+        """Initialize the extension with a flask app"""
         self.app = app
 
         enabled = app.config.setdefault("REQUEST_GUID_ENABLE", True)
@@ -40,7 +47,11 @@ class FlaskRequestGUID:
         return str(uuid.uuid4())
 
     def start_request(self) -> None:
-        """Start the request by getting the request ID from the reqeust headers or setting one."""
+        """
+        Start the request by getting the request ID from the reqeust headers or setting one.
+
+        This method is internally connected to the flask :meth:`~Flask.before_request` method.
+        """
         request_id = request.headers.get(self.header_name, None)
         if request_id is None:
             request_id = self.generate_id()
@@ -48,6 +59,11 @@ class FlaskRequestGUID:
         _app_ctx_stack.top.request_guid = request_id
 
     def finish_request(self, response: Response) -> Response:
-        """Finish the request by adding the request ID to the response headers"""
+        """
+        Finish the request by adding the request ID to the response headers.
+
+        This method is internally connected to the flask :meth:`~Flask.after_request` method,
+        and must return the appropriate response after modifying it.
+        """
         response.headers.setdefault(self.header_name, _app_ctx_stack.top.request_guid)
         return response
